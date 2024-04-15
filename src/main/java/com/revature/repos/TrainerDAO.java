f6f3c9880b608c1b1bc85906b64b3fb8bcc8f14e
pekellogg <peyton846@revature.net>
2024-04-15 16:58:27 -0700

    findTrainerById -> findById and returns Optional

--- src/main/java/com/revature/repos/TrainerDAO.java
+++ src/main/java/com/revature/repos/TrainerDAO.java
@@ -3,8 +3,9 @@ package com.revature.repos;
 import com.revature.models.Trainer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface TrainerDAO extends JpaRepository<Trainer, Integer> {
-    Trainer findTrainerById(int id);
+    Optional<Trainer> findById(int id);
 }

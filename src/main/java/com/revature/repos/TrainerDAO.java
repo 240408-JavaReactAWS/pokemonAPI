c154a5d658da2a00aa37ef273ef55cecd903f25f
pekellogg <peyton846@revature.net>
2024-04-16 10:42:48 -0700

    removed method definitions as unnecessary/redundant

--- src/main/java/com/revature/repos/TrainerDAO.java
+++ src/main/java/com/revature/repos/TrainerDAO.java
@@ -3,10 +3,7 @@ package com.revature.repos;
 import com.revature.models.Trainer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
 
 @Repository
 public interface TrainerDAO extends JpaRepository<Trainer, Integer> {
-    Optional<Trainer> findById(int id);
-    boolean existsById(int id);
 }

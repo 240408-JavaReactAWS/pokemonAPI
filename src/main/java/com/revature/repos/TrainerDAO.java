2888bf8f6433b3b1e7a7bc5b02018bcc5c01c48b
pekellogg <peyton846@revature.net>
2024-04-15 17:23:07 -0700

    adds existsById

--- src/main/java/com/revature/repos/TrainerDAO.java
+++ src/main/java/com/revature/repos/TrainerDAO.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 @Repository
 public interface TrainerDAO extends JpaRepository<Trainer, Integer> {
     Optional<Trainer> findById(int id);
+    boolean existsById(int id);
 }
